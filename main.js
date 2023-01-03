9f2d9210691f2394d371bbf7c5ea04bfeea8ff3b
= <=>
2023-01-04 02:14:02 +0700

    masonry library

--- main.js
+++ main.js
@@ -7,3 +7,9 @@ toggle.addEventListener('click', ()=>{
         col.classList.toggle('collapse-toggle')
     })
 })
+
+// with masonry
+new Masonry('#posts .grid', {
+itemSelector: '.grid-item',
+gutter: 20
+})
