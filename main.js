095a74499374d77850c4a9901458feb5402b6d40
= <=>
2023-01-04 12:00:03 +0700

    slider

--- main.js
+++ main.js
@@ -13,3 +13,31 @@ new Masonry('#posts .grid', {
 itemSelector: '.grid-item',
 gutter: 20
 })
+
+//swiper library
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 5,
+    autoplay:{
+        delay: 3000
+    },
+    //responsive 
+    breakpoints: {
+        '@O' :{
+            slidesPerView:2
+        },
+        //888px
+        '@1.00':{
+            slidesPerView:3
+        },
+        //1110px
+        '@1.25':{
+            slidesPerView:4
+        },
+        //1330px
+        '@1.50':{
+            slidesPerView:5
+        }
+    }
+})
